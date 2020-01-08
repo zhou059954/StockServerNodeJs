67ee4e871e92bf7e5263133bb422749defbcc297
zhou059954 <51169511+zhou059954@users.noreply.github.com>
2020-01-08 18:10:39 +0300

    Update stockController.js

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -51,35 +51,11 @@ router.get('/stock', (req, res) => {
 
 
 router.post('/stock', (req, res) => {
-    var datys = Date.now();
-    var pluss = req.body.plus;
-    var plusStock = new StockPlus({
-        daty: datys,
-        plus: pluss
-    });
-
-    var stocktotal = req.body.total + pluss;
 
     var stock = new Stocks({
         image: req.body.image,
         nom: req.body.nom,
-        total: stocktotal
-    });
-
-    
-    plusStock.save((err, doc) => {
-        if (!err) {
-            res
-                .status(HttpStatus.CREATED)
-                .send(doc);
-        } else {
-            res
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .send({
-                    err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
-                });
-            console.log('Error in Save Plus stock :' + JSON.stringify(err, undefined, 2));
-        }
+        total: req.body.nom
     });
 
     stock.save((err, doc) => {
@@ -104,10 +80,34 @@ router.put('/stock', (req, res) => {
         return res.status(HttpStatus.OK)
             .send(stock_id);
 
+    var datys = Date.now();
+    var pluss = req.body.plus;
+    var plusStock = new StockPlus({
+        daty: datys,
+        plus: pluss
+    });
+
+    var stocktotal = req.body.total + pluss;
+
     var stock = new Stocks({
         image: req.body.image,
         nom: req.body.nom,
-        total: req.body.total
+        total: stocktotal
+    });
+
+    plusStock.save((err, doc) => {
+        if (!err) {
+            res
+                .status(HttpStatus.CREATED)
+                .send(doc);
+        } else {
+            res
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .send({
+                    err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
+                });
+            console.log('Error in Save Plus stock :' + JSON.stringify(err, undefined, 2));
+        }
     });
 
     Stocks.findByIdAndUpdate(stock_id, {
