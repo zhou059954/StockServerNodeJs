df21df062b83f20dab2bdec87a812cb3ed349760
zhou059954 <51169511+zhou059954@users.noreply.github.com>
2020-01-08 11:55:35 +0300

    Update stockController.js

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -51,16 +51,14 @@ router.get('/stock', (req, res) => {
 
 
 router.post('/stock', (req, res) => {
-    /*var daty = Date.now();
-    var plus = req.body.plus;
-    var plus = new StockPlus({
-        daty: daty,
-        plus: plus
+    var datys = Date.now();
+    var pluss = req.body.plus;
+    var plusStock = new StockPlus({
+        daty: datys,
+        plus: pluss
     });
-*/
-    var stocktotal = req.body.total + plus;
 
-
+    var stocktotal = req.body.total + pluss;
 
     var stock = new Stocks({
         image: req.body.image,
@@ -68,7 +66,7 @@ router.post('/stock', (req, res) => {
         total: stocktotal
     });
 
-    /*plus.save((err, doc) => {
+    plusStock.save((err, doc) => {
         if (!err) {
             res
                 .status(HttpStatus.CREATED)
@@ -79,9 +77,9 @@ router.post('/stock', (req, res) => {
                 .send({
                     err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
                 });
-            console.log('Error in Save Testcrud :' + JSON.stringify(err, undefined, 2));
+            console.log('Error in Save Plus stock :' + JSON.stringify(err, undefined, 2));
         }
-    });*/
+    });
 
     stock.save((err, doc) => {
         if (!err) {
@@ -94,7 +92,7 @@ router.post('/stock', (req, res) => {
                 .send({
                     err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
                 });
-            console.log('Error in Save Testcrud :' + JSON.stringify(err, undefined, 2));
+            console.log('Error in Save Stock :' + JSON.stringify(err, undefined, 2));
         }
     });
 });
