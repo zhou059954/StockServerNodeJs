eff4873255e31639e7bc4e278e1673ca7b879fa4
zhou059954 <51169511+zhou059954@users.noreply.github.com>
2020-01-09 02:44:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function () {
   console.log('Node server is running on port ' + app.get('port'));
 });
-//https://m1p6mean-4zo-client.herokuapp.com
+//https://gf-stock.herokuapp.com/ 
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.append('Access-Control-Allow-Origin', 'https://gf-stock.herokuapp.com');
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append("Access-Control-Allow-Headers", "Origin, Accept,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.append('Access-Control-Allow-Credentials', true);
