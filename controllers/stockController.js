f5cfca022d063319078f325a57fce73f7113b283
zhou059954 <51169511+zhou059954@users.noreply.github.com>
2020-01-08 08:38:22 +0300

    test

---
+++ controllers/stockController.js
@@ -0,0 +1,123 @@
+const express = require('express');
+var router = express.Router();
+var HttpStatus = require('http-status-codes');
+var {
+    Stocks
+} = require('../models/stock');
+var ObjectId = require('mongoose').Types.ObjectId;
+
+
+router.get('/stocks', (req, res) => {
+    Stocks.find((err, docs) => {
+        if (!err) {
+            res.status(HttpStatus.OK).send(docs);
+        } else {
+            res
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .send({
+                    err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
+                });
+            console.log('Error in Retriving Testcrud :' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+router.get('/stock', (req, res) => {
+    var user_id = req.param('id');
+    if (!ObjectId.isValid(user_id))
+        return res.status(HttpStatus.OK)
+            .send(user_id);
+
+    Stocks.findById(user_id, (err, doc) => {
+        if (!err) {
+            res
+                .status(HttpStatus.OK)
+                .send(doc);
+        } else {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .send({
+                    err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
+                });
+            console.log('Error in Retriving Testcrud :' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+
+router.post('/stock', (req, res) => {
+    var stock = new Stocks({
+        image: req.body.image,
+        nom: req.body.nom,
+        total: req.body.total
+    });
+    stock.save((err, doc) => {
+        if (!err) {
+            res
+                .status(HttpStatus.CREATED)
+                .send(doc);
+        } else {
+            res
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .send({
+                    err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
+                });
+            console.log('Error in Save Testcrud :' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+router.put('/stock', (req, res) => {
+    var stock_id = req.param('id');
+    if (!ObjectId.isValid(stock_id))
+        return res.status(HttpStatus.OK)
+            .send(stock_id);
+
+    var stock = new Stocks({
+        image: req.body.image,
+        nom: req.body.nom,
+        total: req.body.total
+    });
+
+    Stocks.findByIdAndUpdate(stock_id, {
+        $set: stock
+    }, {
+        new: true
+    }, (err, doc) => {
+        if (!err) {
+            res
+                .status(HttpStatus.OK)
+                .send(doc);
+        } else {
+            res
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .send({
+                    err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
+                });
+            console.log('Error in Edit Testcrud :' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+router.delete('/stock', (req, res) => {
+    var stock_id = req.param('id');
+    if (!ObjectId.isValid(stock_id))
+        return res.status(HttpStatus.OK)
+            .send(stock_id);
+
+    Stocks.findByIdAndDelete(stock_id, (err, doc) => {
+        if (!err) {
+            res
+                .status(HttpStatus.CONFLICT)
+                .send(doc);
+        } else {
+            res
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .send({
+                    err: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
+                });
+            console.log('Error in Delete Testcrud :' + JSON.stringify(err, undefined, 2));
+        }
+    })
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bobyParser = require('body-parser');
 
-const { mongoose } = require('./db.js');
+const {
+  mongoose
+} = require('./db.js');
 
 var testcrudController = require('./controllers/testcrudController.js');
 var utilisateurController = require('./controllers/utilisateurController.js');
@@ -9,6 +11,7 @@ var videosController = require('./controllers/videosController.js');
 var albumsController = require('./controllers/albumsController.js');
 var playlistController = require('./controllers/playlistsController.js');
 var newplaylistController = require('./controllers/newplaylistController.js');
+var stocksController = require('./controllers/stockController.js');
 
 var app = express();
 app.use(bobyParser.json());
@@ -16,19 +19,19 @@ app.use(bobyParser.json());
 /*app.listen(process.env.PORT || 3000, function () {
     console.log('Your node js server is running port :3000');
 });*/
-app.set( 'port', ( process.env.PORT || 3000 ));
+app.set('port', (process.env.PORT || 3000));
 
 // Start node server
-app.listen( app.get( 'port' ), function() {
-  console.log( 'Node server is running on port ' + app.get( 'port' ));
-  });
+app.listen(app.get('port'), function () {
+  console.log('Node server is running on port ' + app.get('port'));
+});
 //https://m1p6mean-4zo-client.herokuapp.com
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append("Access-Control-Allow-Headers", "Origin, Accept,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    res.append('Access-Control-Allow-Credentials', true);
-    next();
+  res.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append("Access-Control-Allow-Headers", "Origin, Accept,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.append('Access-Control-Allow-Credentials', true);
+  next();
 });
 
 app.use('/', utilisateurController);
@@ -37,5 +40,6 @@ app.use('/', videosController);
 app.use('/', albumsController);
 app.use('/', playlistController);
 app.use('/', newplaylistController);
+app.use('/', stocksController);
 app.use('/uploads/videos', express.static('uploads/videos'));
 app.use('/uploads/images', express.static('uploads/images'));
