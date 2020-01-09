3c575907cd84b9ff160016a466845a6566194fb8
zhou059954 <51169511+zhou059954@users.noreply.github.com>
2020-01-10 02:02:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.listen(app.get('port'), function () {
 });
 //https://gf-stock.herokuapp.com/ 
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.append('Access-Control-Allow-Origin', 'https://gf-stock.herokuapp.com');
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append("Access-Control-Allow-Headers", "Origin, Accept,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.append('Access-Control-Allow-Credentials', true);
