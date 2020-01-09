929a93d275124ffc4a62541746b10a530def8aaf
zhou059954 <51169511+zhou059954@users.noreply.github.com>
2020-01-09 20:18:53 +0300

    Update stockController.js

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -32,12 +32,13 @@ var ObjectId = require('mongoose').Types.ObjectId;
 router.get('/stocks', (req, res) => {
     Stocks.aggregate([{
         $project: {
-            image: $image,
-            nom: $nom,
-            total: $total,
-            PU: $PU,
+            _id: "$_id",
+            image: "$image",
+            nom: "$nom",
+            total: "$total",
+            PU: "$PU",
             PT: {
-                $multiply: ["$PU", "$total"]
+                $sum: ["$PU", "$total"]
             }
         }
     }], (err, docs) => {
